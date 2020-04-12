d1175c7520ed233fa4ce992c3236abe0c10d2ee1
dulcetiseduce <swinnaiscool@gmail.com>
2020-04-12 23:51:12 +0200

    OTTIMIZZAZIONE CODICE SUPER ONDA ENERGETICA RISOLVE 12993 ERRORI

--- src/Calcolatrice.java
+++ src/Calcolatrice.java
@@ -1,17 +1,14 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Calcolatrice {
     private JPanel calcolatrice;
 
-    //casella testo e totali casella
-    private JTextField textField1;
-    private JButton AC;
+    //casella testo
+    private JLabel textField1;
     private String calcolo;
     private Double calcolo1;
     private int operazioni;
-    String testo;
+
     //Numeri
     private JButton uno;
     private JButton due;
@@ -23,6 +20,8 @@ public class Calcolatrice {
     private JButton otto;
     private JButton nove;
     private JButton zero;
+
+    //BOTTONI
     private JButton Piu;
     private JButton Meno;
     private JButton Moltiplicazione;
@@ -35,235 +34,173 @@ public class Calcolatrice {
     private JLabel label;
     private JButton delete;
     private JButton segno;
+    private JButton AC;
+
+
+    //TASTI NUMERI
 
     public Calcolatrice() {
-        uno.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent a) {
-                String testo = textField1.getText() + 1;
-                textField1.setText(testo);
-                calcolo = testo;
-            }
+        uno.addActionListener(a -> {
+            String testo = textField1.getText() + 1;
+            textField1.setText(testo);
+            calcolo = testo;
         });
 
-        due.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent a) {
-                String testo = textField1.getText() + 2;
-                textField1.setText(testo);
-                calcolo = testo;
-            }
+        due.addActionListener(a -> {
+            String testo = textField1.getText() + 2;
+            textField1.setText(testo);
+            calcolo = testo;
         });
 
-        tre.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent a) {
-                String testo = textField1.getText() + 3;
-                textField1.setText(testo);
-                calcolo = testo;
-            }
+        tre.addActionListener(a -> {
+            String testo = textField1.getText() + 3;
+            textField1.setText(testo);
+            calcolo = testo;
         });
 
-        quattro.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent a) {
-                String testo = textField1.getText() + 4;
-                textField1.setText(testo);
-                calcolo = testo;
-            }
+        quattro.addActionListener(a -> {
+            String testo = textField1.getText() + 4;
+            textField1.setText(testo);
+            calcolo = testo;
         });
 
-        cinque.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent a) {
-                String testo = textField1.getText() + 5;
-                textField1.setText(testo);
-                calcolo = testo;
-            }
+        cinque.addActionListener(a -> {
+            String testo = textField1.getText() + 5;
+            textField1.setText(testo);
+            calcolo = testo;
         });
 
-        sei.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent a) {
-                String testo = textField1.getText() + 6;
-                textField1.setText(testo);
-                calcolo = testo;
-            }
+        sei.addActionListener(a -> {
+            String testo = textField1.getText() + 6;
+            textField1.setText(testo);
+            calcolo = testo;
         });
 
-        sette.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent a) {
-                String testo = textField1.getText() + 7;
-                textField1.setText(testo);
-                calcolo = testo;
-            }
+        sette.addActionListener(a -> {
+            String testo = textField1.getText() + 7;
+            textField1.setText(testo);
+            calcolo = testo;
         });
 
-        otto.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent a) {
-                String testo = textField1.getText() + 8;
-                textField1.setText(testo);
-                calcolo = testo;
-            }
+        otto.addActionListener(a -> {
+            String testo = textField1.getText() + 8;
+            textField1.setText(testo);
+            calcolo = testo;
         });
 
-        nove.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent a) {
-                String testo = textField1.getText() + 9;
-                textField1.setText(testo);
-                calcolo = testo;
-            }
+        nove.addActionListener(a -> {
+            String testo = textField1.getText() + 9;
+            textField1.setText(testo);
+            calcolo = testo;
         });
 
-        zero.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent a) {
-                String testo = textField1.getText() + 0;
-                textField1.setText(testo);
-                calcolo = testo;
-            }
+        zero.addActionListener(a -> {
+            String testo = textField1.getText() + 0;
+            textField1.setText(testo);
+            calcolo = testo;
         });
 
 
-        Piu.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                operazioni = 1;
-                calcolo1 = Double.parseDouble(calcolo);
-                label.setText(textField1.getText() + "+");
-                textField1.setText("");
-            }
+        Piu.addActionListener(actionEvent -> {
+            operazioni = 1;
+            calcolo1 = Double.parseDouble(calcolo);
+            label.setText(textField1.getText() + "+");
+            textField1.setText("");
         });
-        Meno.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                operazioni = 2;
-                calcolo1 = Double.parseDouble(calcolo);
-                label.setText(textField1.getText() + "-");
-                textField1.setText("");
-            }
+        Meno.addActionListener(actionEvent -> {
+            operazioni = 2;
+            calcolo1 = Double.parseDouble(calcolo);
+            label.setText(textField1.getText() + "-");
+            textField1.setText("");
         });
 
-        Moltiplicazione.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                operazioni = 3;
-                calcolo1 = Double.parseDouble(calcolo);
-                label.setText(textField1.getText() + "*");
-                textField1.setText("");
+        Moltiplicazione.addActionListener(actionEvent -> {
+            operazioni = 3;
+            calcolo1 = Double.parseDouble(calcolo);
+            label.setText(textField1.getText() + "*");
+            textField1.setText("");
 
-            }
         });
 
-        Divisione.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                operazioni = 4;
-                calcolo1 = Double.parseDouble(calcolo);
-                label.setText(textField1.getText() + ":");
-                textField1.setText("");
-            }
+        Divisione.addActionListener(actionEvent -> {
+            operazioni = 4;
+            calcolo1 = Double.parseDouble(calcolo);
+            label.setText(textField1.getText() + ":");
+            textField1.setText("");
         });
 
-        Potenza.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                operazioni = 5;
-                calcolo1 = Double.parseDouble(calcolo);
-                label.setText(textField1.getText() + "^");
-                textField1.setText("");
+        Potenza.addActionListener(actionEvent -> {
+            operazioni = 5;
+            calcolo1 = Double.parseDouble(calcolo);
+            label.setText(textField1.getText() + "^");
+            textField1.setText("");
 
-            }
         });
 
-        radice.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                calcolo1 = Double.parseDouble(calcolo);
-                calcolo1 = Math.sqrt(calcolo1);
-                textField1.setText(String.valueOf(calcolo1));
-            }
+        radice.addActionListener(actionEvent -> {
+            calcolo1 = Double.parseDouble(calcolo);
+            calcolo1 = Math.sqrt(calcolo1);
+            textField1.setText(String.valueOf(calcolo1));
         });
 
-        sin.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                calcolo1 = Double.parseDouble(calcolo);
-                calcolo1 = Math.sin(calcolo1);
-                textField1.setText(String.valueOf(calcolo1));
-            }
+        sin.addActionListener(actionEvent -> {
+            calcolo1 = Double.parseDouble(calcolo);
+            calcolo1 = Math.sin(calcolo1);
+            textField1.setText(String.valueOf(calcolo1));
         });
 
-        cos.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                calcolo1 = Double.parseDouble(calcolo);
-                calcolo1 = Math.cos(calcolo1);
-                textField1.setText(String.valueOf(calcolo1));
-            }
+        cos.addActionListener(actionEvent -> {
+            calcolo1 = Double.parseDouble(calcolo);
+            calcolo1 = Math.cos(calcolo1);
+            textField1.setText(String.valueOf(calcolo1));
         });
 
-        Uguale.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                switch (operazioni) {
-                    case 1:
-                        calcolo1 = calcolo1 + Double.parseDouble(calcolo);
-                        textField1.setText(String.valueOf(calcolo1));
-                        label.setText("0");
-                        break;
-                    case 2:
-                        calcolo1 = calcolo1 - Double.parseDouble(calcolo);
-                        textField1.setText(String.valueOf(calcolo1));
-                        label.setText("0");
-                        break;
-                    case 3:
-                        calcolo1 = calcolo1 * Double.parseDouble(calcolo);
-                        textField1.setText(String.valueOf(calcolo1));
-                        label.setText("0");
-                        break;
-                    case 4:
-                        calcolo1 = calcolo1 / Double.parseDouble(calcolo);
-                        textField1.setText(String.valueOf(calcolo1));
-                        label.setText("0");
-                        break;
-                    case 5:
-                        calcolo1 = Math.pow(calcolo1, Double.parseDouble(calcolo));
-                        textField1.setText(String.valueOf(calcolo1));
-                        label.setText("0");
-                }
+        Uguale.addActionListener(actionEvent -> {
+            switch (operazioni) {
+                case 1:
+                    calcolo1 = calcolo1 + Double.parseDouble(calcolo);
+                    textField1.setText(String.valueOf(calcolo1));
+                    label.setText(" ");
+                    break;
+                case 2:
+                    calcolo1 = calcolo1 - Double.parseDouble(calcolo);
+                    textField1.setText(String.valueOf(calcolo1));
+                    label.setText(" ");
+                    break;
+                case 3:
+                    calcolo1 = calcolo1 * Double.parseDouble(calcolo);
+                    textField1.setText(String.valueOf(calcolo1));
+                    label.setText(" ");
+                    break;
+                case 4:
+                    calcolo1 = calcolo1 / Double.parseDouble(calcolo);
+                    textField1.setText(String.valueOf(calcolo1));
+                    label.setText(" ");
+                    break;
+                case 5:
+                    calcolo1 = Math.pow(calcolo1, Double.parseDouble(calcolo));
+                    textField1.setText(String.valueOf(calcolo1));
+                    label.setText(" ");
             }
         });
 
-        AC.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                textField1.setText("");
-                calcolo = "";
-                label.setText("0");
-            }
+        AC.addActionListener(actionEvent -> {
+            textField1.setText("");
+            calcolo = "";
+            label.setText(" ");
         });
 
-        delete.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                calcolo = calcolo.substring(0, calcolo.length() - 1);
-                textField1.setText(calcolo);
-            }
+        delete.addActionListener(actionEvent -> {
+            calcolo = calcolo.substring(0, calcolo.length() - 1);
+            textField1.setText(calcolo);
         });
 
-        segno.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                if (textField1.getText().contains("-")) {
-                    textField1.setText(textField1.getText().replace("-", ""));
-                } else
-                    textField1.setText("-" + textField1.getText());
-                calcolo = textField1.getText();
-            }
+        segno.addActionListener(actionEvent -> {
+            if (textField1.getText().contains("-")) {
+                textField1.setText(textField1.getText().replace("-", ""));
+            } else
+                textField1.setText("-" + textField1.getText());
+            calcolo = textField1.getText();
         });
 
     }
